a1635b324fee23329f8c96eb1d8a3fb84c5e2547
hasanoffh <ilgarhasanof@gmail.com>
2026-05-28 21:31:11 +0400

    Add 3-password_hash.sh to generate secure SHA-512 password hash via OpenSSL

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl passwd -6 -salt $(openssl rand -base64 16 | cut -c1-16) "$1" | openssl dgst -sha512 > 3_hash.txt
+(echo -n "$1" ; openssl rand -hex 8) | openssl dgst -sha512 > 3_hash.txt
