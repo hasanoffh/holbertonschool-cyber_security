ae930f7637345eeff0dc3db6ed160b6f59bb2d7d
hasanoffh <ilgarhasanof@gmail.com>
2026-06-13 16:45:53 +0400

    Add task 5 subfinder script and output

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d $1 -silent | while read -r sub; do echo $sub; ip=$(dig +short $sub | grep -E "^[0-9.]+$" | head -n1); if [ -n "$ip" ]; then echo "$sub,$ip" >&3; fi; done 3> $1.txt
+subfinder -silent -d $1 -o $1.txt -nW -oI
