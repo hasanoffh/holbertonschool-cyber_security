27e11644ea8fafae0e537c1ae0ce881c31841f70
hasanoffh <ilgarhasanof@gmail.com>
2026-07-04 13:31:23 +0400

    Add task 0 XOR WebSphere decoder script

--- web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
+++ web_application_security/0x01_owasp_top_10/1-xor_decoder.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-# Hədəf hash arqumentini götürürük və {xor} hissəsini təmizləyirik
-CLEAN_HASH=$(echo "$1" | sed 's/{xor}//')
+# Decode IBM WebSphere XOR encoded passwords
 
-# Base64 kodunu açırıq və simvolları növbə ilə tutub 95 ilə XOR edirik
-echo "$CLEAN_HASH" | base64 -d | xxd -p -c1 | while read -r hex
-do
-    if [ -n "$hex" ]
-    then
-        # Hex dəyəri onluq ədədə çeviririk
-        dec=$((16#$hex))
-        # 95 ilə XOR əməliyyatı icra edirik
-        xor_dec=$((dec ^ 95))
-        # Yenidən simvola çevirib çap edirik
-        printf "\\$(printf '%03o' "$xor_dec")"
-    fi
-done
-echo ""
+hash="$1"
+
+clean="${hash#\{xor\}}"
+
+printf "%s" "$clean" | base64 -d | perl -0777 -ne '
+for (unpack("C*", $_)) {
+    print chr($_ ^ 0x5f);
+}
+print "\n";
+'
